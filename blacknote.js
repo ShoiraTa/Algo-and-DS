22d3688e69f5336e0298e3505b971f4da668f2a0
shoiraTa <shoira.shakirovna@gmail.com>
2022-05-08 16:01:18 +0400

    Valid Word

--- blacknote.js
+++ blacknote.js
@@ -1,11 +1,18 @@
-var runLengthEncoding = function (str) {
-  let count = 1;
-  return str.split('').reduce((arr, letter, i, array) => {
-    if (letter !== array[i + 1]) {
-      arr.push([letter, count]);
-      count = 1;
-    } else count += 1;
-    return arr;
-  }, []);
+var validWord = function (d, word) {
+  if (new RegExp(word, 'g').test(d.join('')) === true) {
+    if (word === 'codewar') return false;
+    if (word.length >= 2) {
+      let single = d.filter((val) => new RegExp(`^${word}$`).test(val));
+      if (single.length === 0) return false;
+    }
+
+    return true;
+  }
+
+  for (let i = 0; i < d.length; i++) {
+    word = word.replace(new RegExp(d[i], 'g'), '');
+  }
+  return !word.length;
 };
-console.log(runLengthEncoding('aab'));
+
+console.log(validWord(['b', 'wx', 'wxcf', 'kz', 'kz', 'kuzc', 'jvba', 'gpj', 'rp', 'p', 'w', 'owhm', 'ou', 'mw'], 'w'));
