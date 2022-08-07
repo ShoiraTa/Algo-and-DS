97aa0e413e0bbb6a308b64d2689790ec53eba075
shoiraTa <shoira.shakirovna@gmail.com>
2022-08-07 21:32:55 +0400

    7 kyu How many consecutive numbers are needed?

--- codewars/Ruby/7kyu/7kyu_exercises.rb
+++ codewars/Ruby/7kyu/7kyu_exercises.rb
@@ -1,7 +1,11 @@
+# How many consecutive numbers are needed?
 =begin
-  description goes here
-=end
+ Create the function consecutive(arr) that takes an array of integers and return the minimum number of integers needed to make the contents of arr consecutive from the lowest number to the highest number.
 
-def plus(a,b)
-a+b
+For example:
+If arr contains [4, 8, 6] then the output should be 2 because two numbers need to be added to the array (5 and 7) to make it a consecutive array of numbers from 4 to 8. Numbers in arr will be unique.
+=end
+def consecutive(arr)
+  return 0 if arr.empty?
+  (arr.max - arr.min)+1 - arr.size
 end
--- codewars/Ruby/7kyu/spec/7kyu_exercises_spec.rb
+++ codewars/Ruby/7kyu/spec/7kyu_exercises_spec.rb
@@ -2,9 +2,12 @@ require 'spec_helper'
 require_relative '../7kyu_exercises'
 
 RSpec.describe "7 kyu codewars" do
-  describe 'display current inventory exercise' do
-    it 'test' do
-      expect(plus(1,2)).to eq(3)  
+  describe 'How many consecutive numbers are needed?' do
+    it 'if more than 0' do
+      expect(consecutive([4,8,6])).to eq(2) 
+    end
+    it 'if  0' do
+      expect(consecutive([4,5,6])).to eq(0) 
     end
   end
 end
